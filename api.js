c3e3f3646894b72dce6568d7658237e25e14e310
miyu-code07 <miyukikawaguchi.30207@gmail.com>
2025-07-29 16:50:36 +0900

    学習、作業完了

--- api.js
+++ api.js
@@ -1,8 +1,37 @@
 // setTimeout 引数2つ、1つ目に関数、2つ目に秒数
 
-const timer = setTimeout(function(){
-    alert("Hello!");
-},5000);
+// const timer = setTimeout(function(){
+//     alert("Hello!");
+// },5000);
 
-// clearTimeout タイマー解除
-clearTimeout(timer);
+// // clearTimeout タイマー解除
+// clearTimeout(timer);
+
+// const timer2 = setTimeout(function() {
+//     alert('Hello!');
+// },5000);
+
+// clearTimeout(timer2)
+
+// const $foo =document.querySelectorAll(".bar");
+
+// $foo[0].innerHTML = "<div>Hello!</div>";
+
+
+
+// [変数名]
+
+// $はプレフィックス
+
+// 定数は大文字
+// const DOMEIN = "https://example.com/api";
+
+// true falseを求めるものは 「idxxx」
+// const isLogin = false; ログイン中かどうか
+// const isActive = false;　ボタンが有効になっているか
+
+// 2つ以上の単語を繋げて変数名をつけるとき、2つ目以降の単語の頭文字を大文字にする キャメルケース [JavaScriptでよく使われる]
+
+// 2つ以上の単語を繋げて変数名をつけるとき、単語と単語の間を_アンダースコアで繋げる　スネークケース [Pythonでよく使われる]
+
+
